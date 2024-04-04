d9c3a1a4f95573efaa52d45249a27310a4e45db9
ThanhHai2911 <hailedvs2@gmail.com>
2024-04-04 21:13:42 +0700

    databaseLatestProduct

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,6 @@ class DatabaseSeeder extends Seeder
         $this->call(ProductSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(CategorySeeder::class);
+        $this->call(LatestProductSeerder::class);
     }
 }
