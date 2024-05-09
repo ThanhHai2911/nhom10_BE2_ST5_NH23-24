a1734f2848bde6d59d966eaba0665e3336e6a271
@Khanh08052004 <nguyenduckhanh0805@gmail.com>
2024-05-09 21:12:34 +0700

    xoa san pham khoi gio hang

--- app/Helper/Cart.php
+++ app/Helper/Cart.php
@@ -49,4 +49,16 @@ class Cart
         }
         return $totalQuantity;
     }
+    public function remove($productId)
+    {
+        $cart = session('cart', []); 
+
+        if (array_key_exists($productId, $cart)) {
+            unset($cart[$productId]);
+            session(['cart' => $cart]);
+
+            return redirect()->back()->with('message', 'Sản phẩm đã được xóa khỏi giỏ hàng');
+        }
+        return redirect()->back()->with('error', 'Sản phẩm không tồn tại trong giỏ hàng');
+    }
 }
